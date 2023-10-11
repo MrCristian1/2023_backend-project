8053c6222acf93b820565537b54c712c0c23d4fd
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-10-10 19:31:31 -0500

    Clase 10 octubre

---
+++ resources/views/products/detail.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layout.main')
+
+
+@section('content')
+    <h1 class="text-center m-5">Detalle del producto</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <img src="img/producto1.png" class="img-fluid">
+        </div>
+        <div class="col-md-4">
+            <h1>nombre del producto</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quo, at quae pariatur vero, doloribus quidem, in a officia enim nobis facilis accusantium esse nam deleniti nulla suscipit quisquam impedit!
+            </p>
+            <button type="button" class="btn my-background">Añadir al carrito</button>
+            <h3>$1.000.000</h3>
+
+        </div>
+    </div>
+    </div>
+
+@endsection
+
+
